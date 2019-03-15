09b8022c0abf041a1e196df762dc2797234c4326
Mark Santolucito <mark.santolucito@yale.edu>
2019-03-15 13:38:12 -0400

    fix benchmark

--- benchmarks/synthesis/replaceAllWords.js
+++ benchmarks/synthesis/replaceAllWords.js
@@ -2,11 +2,13 @@ function firstWord(word) {
   return word.substring(0, word.indexOf(0));
 }
 
-function replaceAll(target, search, replacement) {
-  return target.split(search).join(replacement);
+function replaceAll(target, search, replace) {
+  return target.split(search).join(replace);
+//  return target.replace(new RegExp(search, 'g'), replace);
+
 }
 
 function f() {
-  //@pbe (constraint (= (f "Hello World") "Hey World Hey"))
+  //@pbe (constraint (= (f "Hello World Hello") "Hey World Hey"))
   //@pbe (constraint (= (f "Hello Moon") "Hey Moon"))
 }
