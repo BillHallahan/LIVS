06297d655c60f6212751436c994329d6e9810350
mlevatich <maxwell.levatich@yale.edu>
2019-10-12 20:35:29 -0400

    Fixed timeouts in benchmark script, uses new cabal for executing livs

---
+++ benchmarks/synthesis/lia_19/fullGrammar.js
@@ -0,0 +1,18 @@
+function add(m, n) {
+  return m + n;
+}
+
+function square(n) {
+  return n * n;
+}
+
+function cube(n) {
+  return n * n * n;
+}
+
+function quadruple(n) {
+    return square(n) * square(n)
+}
+
+//@pbe (constraint (= (f 2) 16))
+//@pbe (constraint (= (f 3) 81))
--- exe/RunBenchmarks.py
+++ exe/RunBenchmarks.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 
+import os
 import sys
-import subprocess
-import time
+import signal
+from time import monotonic as timer
+from subprocess import Popen, PIPE, TimeoutExpired
+
+# Run command, catching exceptions, timing the execution, and recording output
+def runWithTimeout(cmd, timeout):
+
+    # Start timer and open subprocess
+    start = timer()
+    with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, preexec_fn=os.setsid) as process:
+
+        # Run command with timeout
+        try:
+            out, err = process.communicate(timeout=timeout)
+            output = "ERROR {}".format(err) if err else out.decode("utf-8").split("\n")[-2]
+
+        # Command times out
+        except TimeoutExpired:
+            os.killpg(process.pid, signal.SIGINT)
+            output = "timeout"
+
+    # Stop timer and return output
+    elapsed = timer() - start
+    return elapsed, output
 
 def main():
 
@@ -26,43 +49,24 @@ def main():
     benchmarks = ["benchmarks/synthesis/{}_{}".format(logic, n) for logic, n in filenames]
 
     # Run each benchmark, storing the output in a file
-    print("\ncheck {} to see test results\n".format(sys.argv[1]))
+    print("\ncheck file '{}' to see test results\n".format(sys.argv[1]))
     with open(sys.argv[1], 'w') as outfile:
 
         # Iterate over directories and filenames of benchmarks
         for f in benchmarks:
 
-            # Print the current file
-            print("{}...".format(f))
-            stats = {}
-
             # Run LIVS, catching exceptions and timing the execution
-            start = time.time()
-            try:
-
-                # Collect output from command
-                cmd = "cabal run livs -- --code-file={}/fullGrammar.js".format(f)
-                res = subprocess.check_output(cmd, stderr=subprocess.STDOUT, timeout=timeout, shell=True)
-                output = res.decode("utf-8").split("\n")
-                end = time.time()
-
-            # LIVS throws an exception
-            except subprocess.CalledProcessError as exc:
-                end = time.time()
-                output = "ERROR {}".format(exc.output.decode("utf-8"))
-
-            # LIVS times out
-            except subprocess.TimeoutExpired as exc:
-                end = time.time()
-                output = "timeout"
+            print("{}...".format(f))
+            cmd = "cabal new-run livs -- --code-file={}/fullGrammar.js".format(f)
+            elapsed, output = runWithTimeout(cmd, timeout)
 
             # Write time and output to file
-            time_str = "{0:.3f}s".format(end - start).ljust(7)
-            outfile.write("{}".format(f).ljust(31) + " : {} : {}\n".format(time_str, output))
+            elapsed_str = "{0:.3f}s".format(elapsed).ljust(7)
+            outfile.write("{}".format(f).ljust(31) + " : {} : {}\n".format(elapsed_str, output))
             outfile.flush()
 
     # Reminder
-    print("\ncheck {} to see test results\n".format(sys.argv[1]))
+    print("\ncheck file '{}' to see test results\n".format(sys.argv[1]))
 
 # Entry point
 if __name__ == "__main__":
